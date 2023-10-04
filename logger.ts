9686cbafcad34a0783f025ca06ed89fe93630e70
Alex Mitchell <amitchell@unicef.org>
2023-10-04 16:01:39 +0700

    Allow insert countries & provinces for locale Allow merging countries from object

---
+++ logger.ts
@@ -0,0 +1,6 @@
+// This is for console logs that we want to keep in the codebase
+// Please only use this when actually necessary
+export const logger = (...values) => {
+  // tslint:disable-next-line
+  console.log(...values)
+}

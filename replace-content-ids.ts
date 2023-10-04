49b862dbe60d55b8d0e16ec3705c29c0dc53e603
Alex Mitchell <amitchell@unicef.org>
2023-10-04 16:02:16 +0700

    Add script to replace content Ids

---
+++ replace-content-ids.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import { content } from '@oky/core'
+import { v4 as uuid } from 'uuid'
+import { logger } from './logger'
+
+const localeToUpdate = ''
+
+const outputFilepath = `./packages/core/src/modules/translations/content/${localeToUpdate}.ts`
+
+const replaceContentIds = () => {
+  const updatedContent = { ...content[localeToUpdate] }
+
+  let contentString = JSON.stringify(updatedContent)
+
+  // Only the keys which have .allIds
+  const keys = ['articles', 'categories', 'subCategories', 'quizzes', 'didYouKnows', 'videos']
+
+  keys.forEach((key) => {
+    updatedContent[key]?.allIds.forEach((id) => {
+      contentString = contentString.replaceAll(id, uuid())
+    })
+  })
+
+  const outputString = `
+  import { StaticContent } from '../../../types'
+
+  export const ${localeToUpdate}: StaticContent = ${contentString}`
+
+  fs.writeFileSync(outputFilepath, outputString)
+}
+
+// ========================= Execute ========================= //
+// replaceContentIds()
+
+logger('===== EDIT THIS FILE BEFORE EXECUTING IT =====')

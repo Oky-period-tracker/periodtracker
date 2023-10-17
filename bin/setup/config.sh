763087e47e405a684e9ca634ca6ad157546aa430
Alex Mitchell <amitchell@unicef.org>
2023-10-17 14:16:32 +0700

    Simplify set up scripts via .sh file

---
+++ bin/setup/config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Submodule URLs
+cp bin/modules/urls.sh.dist bin/modules/urls.sh
+
+# env
+cp packages/api/.env.dist packages/api/.env
+cp packages/cms/.env.dist packages/cms/.env
+cp packages/mobile/.env.dist packages/mobile/.env && cp packages/mobile/.env.dist packages/mobile/.env.production
+
+# Mobile
+cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/release.xcconfig && cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/debug.xcconfig
+cp packages/mobile/android/gradle.properties.dist packages/mobile/android/gradle.properties && cp packages/mobile/android/local.properties.dist packages/mobile/android/local.properties
+
+# Redux encryption
+cp packages/components/src/redux/config.ts.dist packages/components/src/redux/config.ts

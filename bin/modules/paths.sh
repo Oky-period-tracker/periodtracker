d1916df98f84fb0b80f199a37ae5bdc7da4b5719
Alex Mitchell <amitchell@unicef.org>
2023-10-17 14:40:13 +0700

    Separate modules by required to run / deploy

--- bin/modules/paths.sh
+++ bin/modules/paths.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Required
-k8s_path=".k8s"
+# Required to run
 assets_path="packages/components/src/assets"
 translations_path="packages/core/src/modules/translations"
+
+# Required to deploy
+k8s_path=".k8s"
 delete_account_path="packages/delete-account"
 
 # Optional

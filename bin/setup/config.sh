161ba184270f0722cac1c9dd6a33bd1e90d99cbf
Alex Mitchell <amitchell@unicef.org>
2023-10-17 15:18:33 +0700

    Improve config.sh

--- bin/setup/config.sh
+++ bin/setup/config.sh
@@ -6,11 +6,16 @@ cp bin/modules/urls.sh.dist bin/modules/urls.sh
 # env
 cp packages/api/.env.dist packages/api/.env
 cp packages/cms/.env.dist packages/cms/.env
-cp packages/mobile/.env.dist packages/mobile/.env && cp packages/mobile/.env.dist packages/mobile/.env.production
+cp packages/mobile/.env.dist packages/mobile/.env
+cp packages/mobile/.env.dist packages/mobile/.env.production
 
-# Mobile
-cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/release.xcconfig && cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/debug.xcconfig
-cp packages/mobile/android/gradle.properties.dist packages/mobile/android/gradle.properties && cp packages/mobile/android/local.properties.dist packages/mobile/android/local.properties
+# iOS
+cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/release.xcconfig
+cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/debug.xcconfig
+
+# Android
+cp packages/mobile/android/gradle.properties.dist packages/mobile/android/gradle.properties
+cp packages/mobile/android/local.properties.dist packages/mobile/android/local.properties
 
 # Redux encryption
 cp packages/components/src/redux/config.ts.dist packages/components/src/redux/config.ts

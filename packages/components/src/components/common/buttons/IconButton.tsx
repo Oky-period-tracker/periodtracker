4b7998fe4008602d5eb2ea99734b2f07ea4cc066
Alex Mitchell <amitchell@unicef.org>
2023-11-17 15:53:27 +0700

    Increase IconButton size on tablet

--- packages/components/src/components/common/buttons/IconButton.tsx
+++ packages/components/src/components/common/buttons/IconButton.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { TouchableOpacity, Image, ImageProps, TouchableOpacityProps } from 'react-native'
+import {
+  TouchableOpacity,
+  Image,
+  ImageProps,
+  TouchableOpacityProps,
+  StyleSheet,
+} from 'react-native'
+import { isTablet } from 'react-native-device-info'
 import { assets } from '../../../assets'
 
 type Props = Omit<ImageProps, 'source'> & {
@@ -11,11 +18,13 @@ type Props = Omit<ImageProps, 'source'> & {
   disabled?: boolean
 }
 
+const defaultSize = isTablet() ? 32 : 20
+
 export const IconButton = ({
   name,
   onPress,
-  width = 20,
-  height = 20,
+  width = defaultSize,
+  height = defaultSize,
   touchableStyle = null,
   disabled = false,
   ...props
@@ -23,10 +32,16 @@ export const IconButton = ({
   return (
     <TouchableOpacity
       disabled={disabled}
-      style={[touchableStyle, { zIndex: 999 }]}
+      style={[styles.default, touchableStyle]}
       onPress={onPress}
     >
       <Image source={assets.static.icons[name]} style={{ width, height }} {...props} />
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  default: {
+    zIndex: 999,
+  },
+})

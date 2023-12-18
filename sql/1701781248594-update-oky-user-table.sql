d7faddd933a0458e854e78c6171ef2adc906c5d4
Alex Mitchell <amitchell@unicef.org>
2023-12-18 13:50:14 +0700

    Default existing user dates to NULL

--- sql/1701781248594-update-oky-user-table.sql
+++ sql/1701781248594-update-oky-user-table.sql
@@ -1,7 +1,7 @@
--- Default existing users to 1970 sign up date
+-- Default existing users NULL
 ALTER TABLE oky_user
-ADD date_signed_up timestamp DEFAULT '2023-01-01 00:00:00.000000',
-ADD date_account_saved timestamp DEFAULT '2023-01-01 00:00:00.000000';
+ADD date_signed_up timestamp DEFAULT NULL,
+ADD date_account_saved timestamp DEFAULT NULL;
 
 -- Change defaults for new sign ups to now
 ALTER TABLE oky_user ALTER COLUMN date_signed_up SET DEFAULT now();

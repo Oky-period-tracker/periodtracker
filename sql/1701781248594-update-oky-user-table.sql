6220a76b3e0bf281f8d15b2e857dab6ee9b102a4
Alex Mitchell <amitchell@unicef.org>
2023-12-19 15:58:11 +0700

    Keep date_signed_up default as NULL

--- sql/1701781248594-update-oky-user-table.sql
+++ sql/1701781248594-update-oky-user-table.sql
@@ -3,6 +3,5 @@ ALTER TABLE oky_user
 ADD date_signed_up timestamp DEFAULT NULL,
 ADD date_account_saved timestamp DEFAULT NULL;
 
--- Change defaults for new sign ups to now
-ALTER TABLE oky_user ALTER COLUMN date_signed_up SET DEFAULT now();
+-- Change default for new users to now
 ALTER TABLE oky_user ALTER COLUMN date_account_saved SET DEFAULT now();

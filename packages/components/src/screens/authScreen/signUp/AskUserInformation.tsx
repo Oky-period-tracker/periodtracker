8cb95b6250dd512a56115e1969ed248195e58ab2
Alex Mitchell <amitchell@unicef.org>
2024-01-02 20:53:20 +0800

    Check store username unique during signup

--- packages/components/src/screens/authScreen/signUp/AskUserInformation.tsx
+++ packages/components/src/screens/authScreen/signUp/AskUserInformation.tsx
@@ -10,6 +10,8 @@ import { httpClient } from '../../../services/HttpClient'
 import { useDebounce } from '../../../hooks/useDebounce'
 import { formHeights } from './FormHeights'
 import { translate } from '../../../i18n'
+import { useSelector } from '../../../redux/useSelector'
+import { hash } from '../../../services/hash'
 
 export function AskUserInformation({ step, heightInner }) {
   const [{ app: state }, dispatch] = useMultiStepForm()
@@ -23,8 +25,19 @@ export function AskUserInformation({ step, heightInner }) {
   const { name, password, passwordConfirm, gender } = state
   const [debouncedName] = useDebounce(name, 500) // to stop fast typing calls
 
+  const storeCredentials = useSelector((s) => s.access.storeCredentials)
+
   React.useEffect(() => {
     let ignore = false
+
+    // Check local store credentials
+    const credentials = storeCredentials[hash(name)]
+    if (credentials) {
+      setNameNotAvailable(true)
+      return
+    }
+
+    // Check online
     async function checkUserNameAvailability() {
       try {
         const response = await httpClient.getUserInfo(name)
@@ -52,7 +65,8 @@ export function AskUserInformation({ step, heightInner }) {
     return (
       password.length >= minPasswordLength &&
       passwordConfirm === password &&
-      name.length >= minNameLength
+      name.length >= minNameLength &&
+      !nameNotAvailable
     )
   }
 

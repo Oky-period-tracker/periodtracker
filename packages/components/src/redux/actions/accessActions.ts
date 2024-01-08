82215b9b45400e19d3fa1c396a2b5840570016a0
Alex Mitchell <amitchell@unicef.org>
2024-01-08 18:05:02 +0800

    Save creds for existing user on new device

--- packages/components/src/redux/actions/accessActions.ts
+++ packages/components/src/redux/actions/accessActions.ts
@@ -1,5 +1,39 @@
 import { createAction } from '../helpers'
 
+export function saveLocalCredentials({
+  appToken,
+  user: {
+    id,
+    name,
+    password,
+    dateOfBirth,
+    gender,
+    location,
+    country,
+    province,
+    secretQuestion,
+    secretAnswer,
+    isGuest,
+  },
+}) {
+  return createAction('SAVE_LOCAL_CREDENTIALS', {
+    appToken,
+    user: {
+      id,
+      name,
+      dateOfBirth,
+      gender,
+      location,
+      country,
+      province,
+      password,
+      secretQuestion,
+      secretAnswer,
+      isGuest,
+    },
+  })
+}
+
 export function setStoreExists(payload: { usernameHash: string }) {
   return createAction('SET_STORE_EXISTS', payload)
 }

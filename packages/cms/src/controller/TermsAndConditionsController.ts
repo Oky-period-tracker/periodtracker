d6d6d4afbfe1d5b27f08e9e8777e60a81f9755d1
Alex Mitchell <amitchell@unicef.org>
2024-01-23 12:57:28 +0800

    Prevent CMS crashes via mobile content fetching

--- packages/cms/src/controller/TermsAndConditionsController.ts
+++ packages/cms/src/controller/TermsAndConditionsController.ts
@@ -14,7 +14,7 @@ export class TermsAndConditionsController {
       where: { lang: request.params.lang },
     })
     const latest = allTermsAndConditionVersions[allTermsAndConditionVersions.length - 1]
-    return latest.json_dump
+    return latest?.json_dump || []
   }
 
   async all(request: Request, response: Response, next: NextFunction) {

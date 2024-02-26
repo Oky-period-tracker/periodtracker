26407b61abf35a4b05da9c772ccf770cc0482ea2
Alex Mitchell <amitchell@unicef.org>
2024-02-26 13:57:28 +0800

    Allow optional restriction of cms languages via submodule

--- packages/cms/src/i18n/options.ts
+++ packages/cms/src/i18n/options.ts
@@ -1,14 +1,23 @@
+// tslint:disable:no-var-requires
 export const cmsTranslations = {
   en: require('./translations/en.json'),
   fr: require('./translations/fr.json'),
   pt: require('./translations/pt.json'),
   ru: require('./translations/ru.json'),
   es: require('./translations/es.json'),
-  //id: require('./translations/id.json'),
-  //mn: require('./translations/mn.json'),
+  ph: require('./translations/ph.json'),
 }
 
-export const cmsLanguages = [
+let chosenLocales = Object.keys(cmsTranslations)
+try {
+  chosenLocales = require('@oky/core/src/modules/translations/cms').cmsLocales
+} catch (e) {
+  //
+}
+
+export const cmsLocales = chosenLocales
+
+const allCmsLanguages = [
   {
     name: 'English',
     locale: 'en',
@@ -25,18 +34,14 @@ export const cmsLanguages = [
     name: 'Русский',
     locale: 'ru',
   },
-//  {
-//    name: 'Indonesian',
-//    locale: 'id',
-//  },
-//  {
-//    name: 'Монгол',
-//    locale: 'mn',
-//  },
   {
     name: 'Español',
     locale: 'es',
   },
+  {
+    name: 'Pilipino',
+    locale: 'ph',
+  },
 ]
 
-export const cmsLocales = cmsLanguages.map((lang) => lang.locale)
+export const cmsLanguages = allCmsLanguages.filter((lang) => cmsLocales.includes(lang.locale))

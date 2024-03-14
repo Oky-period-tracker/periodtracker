205066d9a06797d486a52945e42d322e8d26b31e
Alex Mitchell <amitchell@unicef.org>
2024-03-14 16:58:08 +0800

    Remove duplicated code

--- packages/cms/src/public/scripts/subcategoriesScript.js
+++ packages/cms/src/public/scripts/subcategoriesScript.js
@@ -1,30 +1,3 @@
-$('#categoryModal').on('show.bs.modal', (event) => {
-  var button = $(event.relatedTarget) // Button that triggered the modal
-  var categoryId = button.data('value') // Extract info from data-* attributes
-  $('#errorCat1').hide()
-  $('#errorCat2').hide()
-  $('#errorCat3').hide()
-  $('#errorCatuniq1, #errorCatuniq2, #errorCatuniq3').hide()
-  if (categoryId === 0) {
-    $('.modal-title').text('Insert New Category')
-    $('#colCategory0TableModal').val('')
-    $('#colCategory1TableModal').val('')
-    $('#colCategory2TableModal').val('')
-    $('#itemID').text(0)
-    return
-  }
-  var categories = JSON.parse($('#categoriesJSON').text())
-  var categoryInfo = categories.find((item) => {
-    return item.id === categoryId
-  })
-
-  $('.modal-title').text(categoryInfo.title)
-  $('#colCategory0TableModal').val(categoryInfo.title)
-  $('#colCategory1TableModal').val(categoryInfo.primary_emoji_name)
-  $('#colCategory2TableModal').val(categoryInfo.primary_emoji)
-  $('#itemID').text(categoryId)
-})
-
 $('#subcategoryModal').on('show.bs.modal', (event) => {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var subcategoryId = button.data('value') // Extract info from data-* attributes
@@ -47,47 +20,6 @@ $('#subcategoryModal').on('show.bs.modal', (event) => {
   $('#colSubcategory0TableModal').val(subcategoryInfo.title)
   $('#colSubcategory1TableModal').val(subcategoryInfo.parent_category_id)
   $('#itemID').text(subcategoryId)
-})
-
-$('#btnCategoryConfirm').on('click', () => {
-  const categoryId = $('#itemID').text()
-  const data = {
-    title: $('#colCategory0TableModal').val(),
-    primary_emoji_name: $('#colCategory1TableModal').val(),
-    primary_emoji: $('#colCategory2TableModal').val(),
-  }
-  if (
-    data.title === '' ||
-    data.title.length > 35 ||
-    data.primary_emoji === '' ||
-    data.primary_emoji_name === '' ||
-    data.primary_emoji_name.length > 25
-  ) {
-    $('#errorCat1').show()
-    $('#errorCat2').show()
-    $('#errorCat3').show()
-    return
-  }
-  // if the article ID is 0 we are creating a new entry
-  $.ajax({
-    url: '/categories' + (categoryId === '0' ? '' : '/' + categoryId),
-    type: categoryId === '0' ? 'POST' : 'PUT',
-    data: data,
-    success: (result) => {
-      if (result.duplicate) {
-        if (result.category.title == result.body.title) $('#errorCatuniq1').css('display', 'block')
-        if (result.category.primary_emoji_name == result.body.primary_emoji_name)
-          $('#errorCatuniq2').css('display', 'block')
-        if (result.category.primary_emoji == result.body.primary_emoji)
-          $('#errorCatuniq3').css('display', 'block')
-        return false
-      }
-      location.reload()
-    },
-    error: (error) => {
-      console.log(error)
-    },
-  })
 })
 
 $('#btnSubcategoryConfirm').on('click', () => {
@@ -117,24 +49,6 @@ $('#btnSubcategoryConfirm').on('click', () => {
       console.log(error)
     },
   })
-})
-
-$('.deleteCategory').on('click', (event) => {
-  var button = $(event.currentTarget) // currentTarget is the outer
-  var categoryId = button.data('value') // Extract info from data-* attributes
-  var result = confirm('Are you sure? This will permanently delete the item')
-  if (result) {
-    $.ajax({
-      url: '/categories/' + categoryId,
-      type: 'DELETE',
-      success: (result) => {
-        location.reload()
-      },
-      error: (error) => {
-        console.log(error)
-      },
-    })
-  }
 })
 
 $('.deleteSubcategory').on('click', (event) => {
@@ -167,18 +81,6 @@ function makeUpdateCountdown({ countdownElement, tableElement, maxLength }) {
     tableElement.keyup(updateCountdown)
   })
 }
-
-// makeUpdateCountdown({
-//   countdownElement: $('#countdown1'),
-//   tableElement: $('#colCategory0TableModal'),
-//   maxLength: 35,
-// })
-
-// makeUpdateCountdown({
-//   countdownElement: $('#countdown2'),
-//   tableElement: $('#colCategory1TableModal'),
-//   maxLength: 25,
-// })
 
 makeUpdateCountdown({
   countdownElement: $('#countdown3'),

9f752d7fa17857a0cf5f9466951af78662ddf1a6
Alex Mitchell <amitchell@unicef.org>
2024-03-18 12:51:48 +0800

    Add custom signup fields to app

---
+++ packages/components/src/components/common/AccommodationRequirementSelectItem.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components/native'
+import { assets } from '../../assets/index'
+import { BodyFontSize } from '../../fonts/fontsGlobal'
+import { View, Text } from 'react-native'
+import { translate } from '../../i18n'
+
+export const AccommodationRequirementSelectItem = ({
+  accommodationRequirement,
+  onPress,
+  isActive,
+}) => {
+  return (
+    <Touchable onPress={onPress}>
+      <Block isActive={isActive}>
+        <View style={{ width: '20%' }}>
+          <AccommodationRequirementIcon
+            source={assets.static.icons[accommodationRequirement]}
+            resizeMode="center"
+          />
+        </View>
+        <View style={{ width: '80%', flexDirection: 'row' }}>
+          <Text
+            numberOfLines={2}
+            style={{
+              flexWrap: 'wrap',
+              flex: 1,
+              fontFamily: 'Roboto-Regular',
+              fontSize: BodyFontSize,
+              color: isActive ? 'white' : '#DB307A',
+              textAlign: 'left',
+              paddingLeft: 5,
+            }}
+          >
+            {translate(accommodationRequirement)}
+          </Text>
+        </View>
+      </Block>
+    </Touchable>
+  )
+}
+
+const Block = styled.View`
+  padding: 5px;
+  width: 300;
+  align-items: center;
+  border-radius: 25;
+  flex-direction: row;
+  margin-bottom: 10;
+  background-color: ${(props) => (props.isActive ? '#A4D223' : 'lightgrey')};
+  opacity: ${(props) => (props.isActive ? 1 : 0.4)};
+`
+
+const Touchable = styled.TouchableOpacity``
+
+const AccommodationRequirementIcon = styled.ImageBackground`
+  width: 100%;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+`
--- packages/components/src/screens/authScreen/signUp/AskUserInformation.tsx
+++ packages/components/src/screens/authScreen/signUp/AskUserInformation.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import { Animated } from 'react-native'
+import { Animated, ScrollView } from 'react-native'
 import styled from 'styled-components/native'
 import { Text, TextWithoutTranslation } from '../../../components/common/Text'
 import { TextInput } from '../../../components/common/TextInput'
-import { SegmentControl } from '../../../components/common/SegmentControl'
+import { VerticalSelectBox } from '../../../components/common/VerticalSelectBox'
 import { SignUpFormLayout } from './SignUpFormLayout'
 import { useMultiStepForm, formActions } from '../../../components/common/MultiStepForm'
 import { httpClient } from '../../../services/HttpClient'
 import { useDebounce } from '../../../hooks/useDebounce'
 import { formHeights } from './FormHeights'
 import { translate } from '../../../i18n'
+import { SegmentControl } from '../../../components/common/SegmentControl'
+import { AccommodationRequirementSelectItem } from '../../../components/common/AccommodationRequirementSelectItem'
+import { BodyFontSize, SubTitleFontSize } from '../../../fonts/fontsGlobal'
 import { AppAssets } from '@oky/core'
+import { GenderIdentitySelectItem } from '../../../components/common/GenderIdentitySelectItem'
 
 export function AskUserInformation({ step, heightInner }) {
+  const scrollViewRef = React.useRef(null)
   const [{ app: state }, dispatch] = useMultiStepForm()
   const [notValid, setNotValid] = React.useState(false)
   const [loading, setLoading] = React.useState(false)
@@ -21,8 +26,39 @@ export function AskUserInformation({ step, heightInner }) {
   const minNameLength = 3
   const [usernameError, setUsernameError] = React.useState(false)
   const [passcodeMatchError, setPasscodeMatchError] = React.useState(false)
-  const { name, password, passwordConfirm, gender } = state
+  const {
+    name,
+    password,
+    passwordConfirm,
+    gender,
+    genderIdentity,
+    isPwd,
+    accommodationRequirement,
+    encyclopediaVersion,
+    religion,
+  } = state
   const [debouncedName] = useDebounce(name, 500) // to stop fast typing calls
+
+  const disabilityQs = [
+    'seeing',
+    'hearing',
+    'mobility',
+    'self_care',
+    'communication',
+    'no_difficulty',
+  ]
+
+  const religionOptions = [
+    'undisclosed_religion',
+    'catholic',
+    'islam',
+    'born_again_christian',
+    'iglesia_ni_cristo',
+    'other_religion',
+  ]
+
+  type GenderIdentityOption = 'Yes' | 'No' | 'Other'
+  const genderIdentityOptions: GenderIdentityOption[] = ['Yes', 'No', 'Other']
 
   React.useEffect(() => {
     let ignore = false
@@ -68,6 +104,12 @@ export function AskUserInformation({ step, heightInner }) {
       onSubmit={() => {
         if (!checkValidity()) {
           setNotValid(true)
+
+          // Scroll to the "Password" field if it is empty
+          if (password.length === 0 && scrollViewRef.current) {
+            scrollViewRef.current.scrollToEnd({ animated: true })
+            setNotValid(true)
+          }
           return
         }
 
@@ -90,69 +132,177 @@ export function AskUserInformation({ step, heightInner }) {
           overflow: 'hidden',
         }}
       >
-        {nameNotAvailable && <ErrorMessage>{translate('name_taken_error')}</ErrorMessage>}
-        {usernameError && <ErrorMessage>{translate('username_too_short')}</ErrorMessage>}
-        <TextInput
-          inputStyle={{ color: '#555' }}
-          onChange={(value) => {
-            setUsernameError(false)
-            dispatch({ type: 'change-form-data', inputName: 'name', value })
+        <ScrollView
+          ref={scrollViewRef} // Set the ref for ScrollView
+          contentContainerStyle={{
+            alignItems: 'center',
+            justifyContent: 'center',
           }}
-          label="enter_name"
-          isValid={name.length >= minNameLength}
-          hasError={notValid && !(name.length >= minNameLength)}
-          showInfoButton={true}
-          infoAccessibilityLabel={translate('name_info_label')}
-          value={name}
-          errorHeading="name"
-          errorContent="name_info_label"
-        />
+          showsVerticalScrollIndicator={true}
+        >
+          {nameNotAvailable && <ErrorMessage>{translate('name_taken_error')}</ErrorMessage>}
+          {usernameError && <ErrorMessage>{translate('username_too_short')}</ErrorMessage>}
+          <TextInput
+            inputStyle={{ color: '#555' }}
+            onChange={(value) => {
+              setUsernameError(false)
+              dispatch({ type: 'change-form-data', inputName: 'name', value })
+            }}
+            label="enter_name"
+            isValid={name.length >= minNameLength}
+            hasError={notValid && !(name.length >= minNameLength)}
+            showInfoButton={true}
+            infoAccessibilityLabel={translate('name_info_label')}
+            value={name}
+            errorHeading="name"
+            errorContent="name_info_label"
+          />
 
-        <GenderText>your_gender</GenderText>
-        <Row>
-          {genders.map((value) => {
-            return (
-              <SegmentControl
+          <GenderText>your_gender</GenderText>
+          <Row>
+            {genders.map((value) => {
+              return (
+                <SegmentControl
+                  key={value}
+                  option={value}
+                  isActive={gender === value}
+                  onPress={() => dispatch({ type: 'change-form-data', inputName: 'gender', value })}
+                />
+              )
+            })}
+          </Row>
+
+          <GenderIdentityText>your_gender_identity</GenderIdentityText>
+          <Row>
+            {genderIdentityOptions.map((value) => {
+              return (
+                <GenderIdentitySelectItem
+                  key={value}
+                  genderIdentity={value}
+                  isActive={genderIdentity === value}
+                  onPress={() =>
+                    dispatch({ type: 'change-form-data', inputName: 'genderIdentity', value })
+                  }
+                />
+              )
+            })}
+          </Row>
+
+          <DisabilityText>disability_question</DisabilityText>
+
+          <Column>
+            {disabilityQs.map((value) => (
+              <AccommodationRequirementSelectItem
                 key={value}
-                option={value}
-                isActive={gender === value}
-                onPress={() => dispatch({ type: 'change-form-data', inputName: 'gender', value })}
+                accommodationRequirement={value}
+                isActive={accommodationRequirement === value}
+                onPress={() =>
+                  dispatch({
+                    type: 'change-form-data',
+                    inputName: 'accommodationRequirement',
+                    value,
+                  })
+                }
               />
-            )
-          })}
-        </Row>
-        <TextInput
-          onChange={(value) => dispatch({ type: 'change-form-data', inputName: 'password', value })}
-          label="password"
-          secureTextEntry={true}
-          showInfoButton={true}
-          isValid={password.length >= minPasswordLength}
-          hasError={notValid && !(password.length >= minPasswordLength)}
-          value={password}
-          errorHeading="password_error_heading"
-          errorContent="password_error_content"
-        />
-        <TextInput
-          onChange={(value) => {
-            setPasscodeMatchError(false)
-            dispatch({ type: 'change-form-data', inputName: 'passwordConfirm', value })
-          }}
-          label="confirm_password"
-          secureTextEntry={true}
-          showInfoButton={false}
-          isValid={passwordConfirm.length >= minPasswordLength && passwordConfirm === password}
-          hasError={
-            notValid &&
-            !(passwordConfirm.length >= minPasswordLength && passwordConfirm === password)
-          }
-          value={passwordConfirm}
-        />
-        {passcodeMatchError && <ErrorMessage>{translate('passcodes_mismatch')}</ErrorMessage>}
+            ))}
+          </Column>
+
+          <ReligionText>religion_question</ReligionText>
+          <VerticalSelectBox
+            items={religionOptions.map((option) => (option ? option : ''))}
+            containerStyle={{
+              height: 45,
+              borderRadius: 22.5,
+            }}
+            height={45}
+            maxLength={20}
+            buttonStyle={{ right: 5, bottom: 7 }}
+            onValueChange={(value) => {
+              dispatch({ type: 'change-form-data', inputName: 'religion', value })
+
+              if (value === 'islam') {
+                dispatch({
+                  type: 'change-form-data',
+                  inputName: 'encyclopediaVersion',
+                  value: 'Yes',
+                })
+              } else {
+                dispatch({
+                  type: 'change-form-data',
+                  inputName: 'encyclopediaVersion',
+                  value: 'No',
+                })
+              }
+            }}
+            hasError={true} // this is to permanently display the i button
+            errorHeading="religion_perspective_heading"
+            errorContent="religion_perspective_content"
+          />
+
+          {religion !== 'islam' && religion === 'undisclosed_religion' && (
+            <>
+              <EncyclopediaVersionText>encyclopedia_version_question</EncyclopediaVersionText>
+              <VerticalSelectBox
+                items={['No', 'Yes'].map((option) => (option ? option : ''))}
+                containerStyle={{
+                  height: 45,
+                  borderRadius: 22.5,
+                }}
+                height={45}
+                maxLength={20}
+                buttonStyle={{ right: 5, bottom: 7 }}
+                onValueChange={(value) =>
+                  dispatch({ type: 'change-form-data', inputName: 'encyclopediaVersion', value })
+                }
+                hasError={encyclopediaVersion === 'Yes'}
+                errorHeading="islamic_perspective_heading"
+                errorContent="islamic_perspective_content"
+              />
+            </>
+          )}
+
+          <TextInput
+            onChange={(value) =>
+              dispatch({ type: 'change-form-data', inputName: 'password', value })
+            }
+            label="password"
+            secureTextEntry={true}
+            showInfoButton={true}
+            isValid={password.length >= minPasswordLength}
+            hasError={notValid && !(password.length >= minPasswordLength)}
+            value={password}
+            errorHeading="password_error_heading"
+            errorContent="password_error_content"
+          />
+          <TextInput
+            onChange={(value) => {
+              setPasscodeMatchError(false)
+              dispatch({ type: 'change-form-data', inputName: 'passwordConfirm', value })
+            }}
+            label="confirm_password"
+            secureTextEntry={true}
+            showInfoButton={false}
+            isValid={passwordConfirm.length >= minPasswordLength && passwordConfirm === password}
+            hasError={
+              notValid &&
+              !(passwordConfirm.length >= minPasswordLength && passwordConfirm === password)
+            }
+            value={passwordConfirm}
+          />
+          {passcodeMatchError && <ErrorMessage>{translate('passcodes_mismatch')}</ErrorMessage>}
+        </ScrollView>
       </Container>
     </SignUpFormLayout>
   )
 }
 
+const Column = styled.View`
+  width: 80%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  margin-bottom: 10px;
+`
 const Row = styled.View`
   width: 80%;
   flex-direction: row;
@@ -169,8 +319,9 @@ const Container = styled.View`
 
 const GenderText = styled(Text)`
   font-family: Roboto-Regular;
-  font-size: 14;
-  margin-bottom: 5px;
+  font-size: ${SubTitleFontSize};
+  margin-bottom: 10px;
+  margin-top: 20px;
   color: #28b9cb;
 `
 
@@ -179,10 +330,38 @@ const ErrorMessage = styled(TextWithoutTranslation)`
   margin-top: 10px;
   color: red;
 `
-const PasswordDescription = styled(Text)`
-  width: 95%;
-  text-align: justify;
-  font-size: 12;
+
+const GenderIdentityText = styled(Text)`
+  font-family: Roboto-Regular;
+  font-size: ${SubTitleFontSize};
   margin-bottom: 10px;
+  margin-top: 20px;
   color: #28b9cb;
+  text-align: center;
+`
+
+const DisabilityText = styled(Text)`
+  font-family: Roboto-Regular;
+  font-size: ${SubTitleFontSize};
+  margin-bottom: 10px;
+  margin-top: 20px;
+  color: #28b9cb;
+  text-align: center;
+`
+
+const ReligionText = styled(Text)`
+  font-family: Roboto-Regular;
+  font-size: ${SubTitleFontSize};
+  margin-bottom: 10px;
+  margin-top: 20px;
+  color: #28b9cb;
+`
+
+const EncyclopediaVersionText = styled(Text)`
+  font-family: Roboto-Regular;
+  font-size: ${BodyFontSize};
+  margin-bottom: 10px;
+  margin-top: 40px;
+  color: #28b9cb;
+  text-align: center;
 `

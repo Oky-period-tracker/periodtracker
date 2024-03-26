e1d6d52615af5d5c597b90893b9ac2e587229e8f
Alex Mitchell <amitchell@unicef.org>
2024-03-26 15:02:29 +0800

    Alter help_center table and create help_center_attributes table

---
+++ sql/migrations/1711427004112-help-center.sql
@@ -0,0 +1,22 @@
+CREATE SEQUENCE periodtracker.help_center_sorting_key INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775806 START 1 CACHE 1;
+
+ALTER TABLE help_center
+ADD "province" text DEFAULT NULL,
+ADD "city" text DEFAULT NULL,
+ADD "isAvailableNationwide" boolean DEFAULT true,
+ADD "primaryAttributeId" integer DEFAULT NULL,
+ADD "otherAttributes" text DEFAULT NULL,
+ADD "isActive" boolean DEFAULT false,
+ADD "sortingKey" integer DEFAULT nextval('periodtracker.help_center_sorting_key') NOT NULL;
+
+CREATE SEQUENCE periodtracker.help_center_attributes_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775806 START 1 CACHE 1;
+
+CREATE TABLE "periodtracker"."help_center_attributes" (
+    "id" integer DEFAULT nextval('periodtracker.help_center_attributes_id_seq') NOT NULL,
+    "attributeName" character varying NOT NULL,
+    "description" character varying NOT NULL,
+    "isActive" boolean DEFAULT false,
+    CONSTRAINT "PK_help_center_attributes" PRIMARY KEY ("id")
+) WITH (oids = false);
+
+

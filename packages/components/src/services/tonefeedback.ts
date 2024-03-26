2bd7c8bf6189b3f73d85734cbe778a518d056ceb
Alex Mitchell <amitchell@unicef.org>
2024-03-26 11:57:18 +0800

    Add HAPTIC_AND_SOUND_ENABLED to env

--- packages/components/src/services/tonefeedback.ts
+++ packages/components/src/services/tonefeedback.ts
@@ -1,9 +1,14 @@
 import Sound from 'react-native-sound'
 import HapticFeedback from 'react-native-haptic-feedback'
 import _ from 'lodash'
+import { HAPTIC_AND_SOUND_ENABLED } from '../config'
 
 export const hapticAndSoundFeedback = _.throttle(
   (toneName: 'bubble' | 'general' | 'warning' | 'close' | 'key') => {
+    if (!HAPTIC_AND_SOUND_ENABLED) {
+      return
+    }
+
     const options = {
       enableVibrateFallback: true,
       ignoreAndroidSystemSettings: true,

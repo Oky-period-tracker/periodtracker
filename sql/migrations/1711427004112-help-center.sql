c2b4986eb4885215da1c8c69c78f45aa56ed382a
Alex Mitchell <amitchell@unicef.org>
2024-04-08 11:12:03 +0800

    Update hc migration sql

--- sql/migrations/1711427004112-help-center.sql
+++ sql/migrations/1711427004112-help-center.sql
@@ -1,8 +1,8 @@
 CREATE SEQUENCE periodtracker.help_center_sorting_key INCREMENT 1 MINVALUE 1 MAXVALUE 9223372036854775806 START 1 CACHE 1;
 
 ALTER TABLE help_center
-ADD "province" text DEFAULT NULL,
-ADD "city" text DEFAULT NULL,
+ADD "location" text DEFAULT NULL,
+ADD "place" text DEFAULT NULL,
 ADD "isAvailableNationwide" boolean DEFAULT true,
 ADD "primaryAttributeId" integer DEFAULT NULL,
 ADD "otherAttributes" text DEFAULT NULL,

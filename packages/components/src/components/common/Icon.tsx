a6427e17034ac88921ba74890bf60ec1479ee192
Alex Mitchell <amitchell@unicef.org>
2024-04-18 11:14:16 +0800

    Add Icon prop types

--- packages/components/src/components/common/Icon.tsx
+++ packages/components/src/components/common/Icon.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { ImageProps } from 'react-native'
 import styled from 'styled-components/native'
 
-export const Icon = ({ source, style = null }) => {
+export const Icon = ({
+  source,
+  style = null,
+}: {
+  source: ImageProps['source']
+  style?: ImageProps['style']
+}) => {
   return <ImageContainer resizeMode="contain" source={source} style={style} />
 }
 

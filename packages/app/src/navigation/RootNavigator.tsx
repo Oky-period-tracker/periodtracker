d65872775af6decaacbc4b47c6aeaeee3c828c21
Alex Mitchell <amitchell@unicef.org>
2024-05-01 13:59:18 +0800

    Convert root navigation to tabs

--- packages/app/src/navigation/RootNavigator.tsx
+++ packages/app/src/navigation/RootNavigator.tsx
@@ -1,25 +1,64 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import ProfileStack from "./stacks/ProfileStack";
 import HomeStack from "./stacks/HomeStack";
 import EncyclopediaStack from "./stacks/EncyclopediaStack";
 import SettingsStack from "./stacks/SettingsStack";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const options = {
+  tabBarShowLabel: false,
+  headerShown: false,
+};
 
 function RootNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={"HomeStack"}>
-        <Stack.Screen name={"ProfileStack"} component={ProfileStack} />
-        <Stack.Screen name={"HomeStack"} component={HomeStack} />
-        <Stack.Screen
+      <Tab.Navigator initialRouteName={"HomeStack"}>
+        <Tab.Screen
+          name={"ProfileStack"}
+          component={ProfileStack}
+          options={{
+            ...options,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="user" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name={"HomeStack"}
+          component={HomeStack}
+          options={{
+            ...options,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="home" color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
           name={"EncyclopediaStack"}
           component={EncyclopediaStack}
+          options={{
+            ...options,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="book" color={color} />
+            ),
+          }}
         />
-        <Stack.Screen name={"SettingsStack"} component={SettingsStack} />
-      </Stack.Navigator>
+        <Tab.Screen
+          name={"SettingsStack"}
+          component={SettingsStack}
+          options={{
+            ...options,
+            tabBarIcon: ({ color }) => (
+              <FontAwesome size={28} name="gear" color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }

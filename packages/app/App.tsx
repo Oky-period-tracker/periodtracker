b6a5b76737662b53c822fbb365696b008944d447
Alex Mitchell <amitchell@unicef.org>
2024-05-01 16:05:26 +0800

    Lock orientation on Mobile but not Tablet

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,7 +1,25 @@
 import * as React from "react";
 import RootNavigator from "./src/navigation/RootNavigator";
+import { IS_TABLET } from "./src/services/tablet";
+import {
+  OrientationLock,
+  lockAsync,
+  unlockAsync,
+} from "expo-screen-orientation";
 
 function App() {
+  React.useEffect(() => {
+    if (IS_TABLET) {
+      return;
+    }
+
+    lockAsync(OrientationLock.PORTRAIT_UP);
+
+    return () => {
+      unlockAsync();
+    };
+  }, []);
+
   return <RootNavigator />;
 }
 
---
+++ packages/app/src/services/tablet.ts
@@ -0,0 +1,3 @@
+import { deviceType } from "expo-device";
+
+export const IS_TABLET = deviceType === 2;

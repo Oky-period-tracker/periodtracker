026bee88c98c0a34c72049349876dc94ffbb1e2b
Alex Mitchell <amitchell@unicef.org>
2024-05-04 16:23:14 +0800

    Encyclopedia accordion WIP

---
+++ packages/app/src/screens/EncyclopediaScreen/components/Accordion.tsx
@@ -0,0 +1,93 @@
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useToggle } from "../../../hooks/useToggle";
+import { data } from "../data";
+import { UntouchableButton } from "../../../components/Button";
+import { useNavigation } from "@react-navigation/native";
+
+function Accordion() {
+  return (
+    <ScrollView style={styles.accordionContainer}>
+      {data.categories.allIds.map((categoryId) => (
+        <AccordionItem categoryId={categoryId} />
+      ))}
+    </ScrollView>
+  );
+}
+
+export default Accordion;
+
+const AccordionItem = ({ categoryId }: { categoryId: string }) => {
+  const navigation = useNavigation() as any; // @TODO: Fixme
+  const [expanded, toggleExpanded] = useToggle();
+
+  const category = data.categories.byId[categoryId];
+  const subcategories = category.subCategories.map(
+    (subcategoryId) => data.subCategories.byId[subcategoryId]
+  );
+
+  return (
+    <>
+      <TouchableOpacity style={styles.category} onPress={toggleExpanded}>
+        <Text style={styles.categoryName}>{category.name}</Text>
+        <UntouchableButton
+          status={"basic"}
+          style={styles.categoryEmoji}
+        ></UntouchableButton>
+      </TouchableOpacity>
+      {expanded
+        ? subcategories.map((subcategory) => (
+            <TouchableOpacity
+              style={styles.subcategory}
+              onPress={() => navigation.navigate("ArticlesScreen")}
+            >
+              <Text>{subcategory.name}</Text>
+            </TouchableOpacity>
+          ))
+        : null}
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  accordionContainer: {
+    width: "90%",
+    height: "100%",
+  },
+  category: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    height: 80,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    marginVertical: 4,
+    paddingHorizontal: 24,
+  },
+  categoryName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  categoryEmoji: {
+    width: 40,
+    height: 40,
+  },
+  subcategory: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignSelf: "flex-end",
+    width: "90%",
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    marginVertical: 4,
+    paddingHorizontal: 24,
+  },
+});
--- packages/app/src/screens/EncyclopediaScreen/index.tsx
+++ packages/app/src/screens/EncyclopediaScreen/index.tsx
@@ -1,10 +1,20 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { View } from "react-native";
+
+import Accordion from "./components/Accordion";
 
 function EncyclopediaScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>EncyclopediaScreen</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "red",
+        padding: 12,
+      }}
+    >
+      <Accordion />
     </View>
   );
 }

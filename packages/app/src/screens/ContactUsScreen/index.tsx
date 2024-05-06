48eded5d371625276d4af5a36def93d9d0381448
Alex Mitchell <amitchell@unicef.org>
2024-05-06 15:05:46 +0800

    Contact Us screen

--- packages/app/src/screens/ContactUsScreen/index.tsx
+++ packages/app/src/screens/ContactUsScreen/index.tsx
@@ -1,12 +1,51 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { TextInput, StyleSheet, View } from "react-native";
+import { Button } from "../../components/Button";
+import { Screen } from "../../components/Screen";
 
-function ContactUsScreen({ navigation }) {
+function ContactUsScreen() {
+  const [reason, setReason] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>ContactUsScreen</Text>
-    </View>
+    <Screen>
+      <View style={styles.container}>
+        <TextInput
+          placeholder="reason"
+          style={styles.input}
+          onChangeText={setReason}
+          value={reason}
+        />
+        <TextInput
+          placeholder="message"
+          style={styles.input}
+          onChangeText={setMessage}
+          value={message}
+          multiline={true}
+          numberOfLines={5}
+        />
+        <Button>Send</Button>
+      </View>
+    </Screen>
   );
 }
 
 export default ContactUsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    width: "100%",
+    padding: 24,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#f0f0f0",
+    padding: 8,
+    margin: 8,
+    width: "100%",
+    borderRadius: 8,
+  },
+});

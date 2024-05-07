52ecccbeb0c47b7ede93f9f3d3107449ca1cc8a7
Alex Mitchell <amitchell@unicef.org>
2024-05-07 17:39:26 +0800

    AvatarAndTheme Screen UI WIP

--- packages/app/src/screens/AvatarAndThemeScreen/index.tsx
+++ packages/app/src/screens/AvatarAndThemeScreen/index.tsx
@@ -1,12 +1,79 @@
 import * as React from "react";
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Screen } from "../../components/Screen";
+import { UntouchableButton } from "../../components/Button";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 function AvatarAndThemeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>AvatarAndThemeScreen</Text>
-    </View>
+    <Screen style={styles.screen}>
+      <View style={styles.avatars}>
+        <View style={styles.avatar}>
+          <UntouchableButton style={styles.check}>
+            <FontAwesome size={12} name={"check"} color={"#fff"} />
+          </UntouchableButton>
+        </View>
+        <View style={styles.avatar}></View>
+        <View style={styles.avatar}></View>
+        <View style={styles.avatar}></View>
+        <View style={styles.avatar}></View>
+        <View style={styles.avatar}></View>
+        <View style={styles.avatar}></View>
+      </View>
+
+      <View style={styles.themes}>
+        <View style={styles.theme}></View>
+        <View style={styles.theme}></View>
+        <View style={styles.theme}></View>
+        <View style={styles.theme}></View>
+      </View>
+    </Screen>
   );
 }
 
 export default AvatarAndThemeScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  avatars: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    marginBottom: 24,
+  },
+  avatar: {
+    backgroundColor: "white",
+    width: 100,
+    height: 100,
+    borderRadius: 20,
+    margin: 4,
+  },
+  themes: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    flexWrap: "wrap",
+  },
+  theme: {
+    backgroundColor: "white",
+    minWidth: 100,
+    height: 100,
+    borderRadius: 20,
+    flexBasis: "40%",
+    margin: 8,
+  },
+  check: {
+    height: 24,
+    width: 24,
+    position: "absolute",
+    top: 8,
+    left: 8,
+  },
+});

48b0e4a478b128e6a21394b908b00727accf26e7
Alex Mitchell <amitchell@unicef.org>
2024-05-18 13:24:05 +0800

    Carousel WIP

--- packages/app/src/screens/MainScreen/index.tsx
+++ packages/app/src/screens/MainScreen/index.tsx
@@ -1,14 +1,29 @@
 import * as React from "react";
-import { View, Text } from "react-native";
-import { Button } from "../../components/Button";
+import { View, Text, StyleSheet, Platform, StatusBar } from "react-native";
+import Carousel from "../../components/Carousel";
+// import { Button } from "../../components/Button";
+
+const data = [{}, {}, {}, {}, {}, {}, {}, {}];
 
 function MainScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>MainScreen</Text>
-      <Button onPress={() => navigation.navigate("Calendar")}>Calendar</Button>
+    <View style={styles.carouselContainer}>
+      <Text style={styles.text}>Image Carousel Square</Text>
+      <Carousel data={data} pagination={true} />
     </View>
   );
 }
 
 export default MainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "white",
+  },
+  text: { textAlign: "center", color: "black", marginBottom: 10 },
+  carouselContainer: {
+    marginBottom: 20,
+  },
+});

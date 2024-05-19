502194ed33be5f95f56b788a7cce809b2b6b69b5
Alex Mitchell <amitchell@unicef.org>
2024-05-19 12:33:02 +0800

    Refactor Access screen

--- packages/app/src/screens/AccessScreen/index.tsx
+++ packages/app/src/screens/AccessScreen/index.tsx
@@ -1,56 +1,66 @@
 import * as React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Button } from "../../components/Button";
 import { Screen } from "../../components/Screen";
 import { Hr } from "../../components/Hr";
 
+export type AccessRowProps = {
+  title: string;
+  description: string;
+  buttonText: string;
+  onPress?: () => void;
+};
+
+export const AccessRow = ({
+  title,
+  description,
+  buttonText,
+  onPress,
+}: AccessRowProps) => {
+  return (
+    <View style={styles.row}>
+      <View style={styles.rowLeft}>
+        <Text style={styles.title}>{title}</Text>
+        <Text>{description}</Text>
+      </View>
+      <View style={styles.rowRight}>
+        <Button onPress={onPress}>{buttonText}</Button>
+      </View>
+    </View>
+  );
+};
+
 function AccessScreen({ navigation }) {
+  const rows: AccessRowProps[] = [
+    {
+      title: "Language",
+      description: "Change the language Oky uses",
+      buttonText: "English",
+    },
+    {
+      title: "Tutorial",
+      description: "Get instructions on how to use Oky",
+      buttonText: "Launch",
+    },
+    {
+      title: "Share",
+      description: "Share Oky with your friends",
+      buttonText: "Share",
+    },
+  ];
+
   return (
     <Screen>
       <View style={styles.container}>
-        <TouchableOpacity
-          style={styles.segment}
-          onPress={() => navigation.navigate("About")}
-        >
-          <View style={styles.segmentLeft}>
-            <Text style={styles.title}>Language</Text>
-            <Text>Change the language Oky uses:</Text>
-          </View>
-          <View style={styles.segmentRight}>
-            <Button>English</Button>
-          </View>
-        </TouchableOpacity>
-
-        <Hr />
-
-        <TouchableOpacity
-          style={styles.segment}
-          onPress={() => navigation.navigate("Terms")}
-        >
-          <View style={styles.segmentLeft}>
-            <Text style={styles.title}>Tutorial</Text>
-            <Text>Get instructions on how to use Oky</Text>
-          </View>
-          <View style={styles.segmentRight}></View>
-          <View style={styles.segmentRight}>
-            <Button>Launch</Button>
-          </View>
-        </TouchableOpacity>
-
-        <Hr />
-
-        <TouchableOpacity
-          style={styles.segment}
-          onPress={() => navigation.navigate("Privacy")}
-        >
-          <View style={styles.segmentLeft}>
-            <Text style={styles.title}>Share</Text>
-            <Text>Share Oky with your friends</Text>
-          </View>
-          <View style={styles.segmentRight}>
-            <Button>Share</Button>
-          </View>
-        </TouchableOpacity>
+        {rows.map((props, i) => {
+          const isLast = i !== rows.length - 1;
+          return (
+            <>
+              <AccessRow key={`access-${i}`} {...props} />
+              {isLast ? <Hr /> : null}
+            </>
+          );
+        })}
       </View>
     </Screen>
   );
@@ -65,17 +75,19 @@ const styles = StyleSheet.create({
     width: "100%",
     overflow: "hidden",
   },
-  segment: {
+  row: {
     height: 100,
     width: "100%",
     flexDirection: "row",
     padding: 24,
   },
-  segmentLeft: {
+  rowLeft: {
     flexBasis: "30%",
     justifyContent: "center",
+    flex: 1,
+    padding: 8,
   },
-  segmentRight: {
+  rowRight: {
     alignItems: "flex-end",
     justifyContent: "center",
     flex: 1,

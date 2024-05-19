e1884254f6a4a706363378d9e6793aa8c943a1ab
Alex Mitchell <amitchell@unicef.org>
2024-05-19 23:12:52 +0800

    Set max width for screen content

--- packages/app/src/components/Screen.tsx
+++ packages/app/src/components/Screen.tsx
@@ -2,15 +2,23 @@ import { StyleSheet, View, ViewProps } from "react-native";
 
 export const Screen = ({ children, style, ...props }: ViewProps) => {
   return (
-    <View style={[styles.screen, style]} {...props}>
-      {children}
+    <View style={styles.wrapper}>
+      <View style={[styles.screen, style]} {...props}>
+        {children}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: "center",
+    flex: 1,
+  },
   screen: {
     flex: 1,
+    maxWidth: 800,
+    width: "100%",
     alignItems: "center",
     paddingHorizontal: 12,
   },

6da914915a29ab4b96d1745eb3fa87cd98d7daf7
Alex Mitchell <amitchell@unicef.org>
2024-05-19 15:22:37 +0800

    Remove unused

--- packages/app/src/navigation/components/Header.tsx
+++ packages/app/src/navigation/components/Header.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { Platform, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Button } from "../../components/Button";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";

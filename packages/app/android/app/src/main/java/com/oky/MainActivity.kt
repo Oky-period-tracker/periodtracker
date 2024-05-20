e8dfec7044c0595f21408f9e5fb32987200aa02f
Alex Mitchell <amitchell@unicef.org>
2024-05-20 18:14:32 +0800

    Android config attempt

--- packages/app/android/app/src/main/java/com/oky/MainActivity.kt
+++ packages/app/android/app/src/main/java/com/oky/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.alex
+package com.oky
 
 import android.os.Build
 import android.os.Bundle
--- packages/app/android/app/src/main/java/com/oky/MainApplication.kt
+++ packages/app/android/app/src/main/java/com/oky/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.alex
+package com.oky
 
 import android.app.Application
 import android.content.res.Configuration

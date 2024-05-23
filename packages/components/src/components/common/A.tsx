06850944318c3a964d4aef729429f130f82bb645
Alex Mitchell <amitchell@unicef.org>
2024-05-23 14:37:28 +0800

    Add anchor link component

---
+++ packages/components/src/components/common/A.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  Linking,
+} from 'react-native'
+
+export const A = ({
+  href,
+  style,
+  ...props
+}: TouchableOpacityProps & {
+  href: string
+}) => {
+  const onPressLink = () => openURL(href)
+
+  const children = props.children ? (
+    typeof props.children === 'string' ? (
+      <Text style={styles.text}>{props.children}</Text>
+    ) : (
+      props.children
+    )
+  ) : null
+
+  return (
+    <TouchableOpacity style={style} onPress={onPressLink}>
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+const openURL = (href, target = '_blank') => {
+  let url = href
+  if (!url.includes('https://')) {
+    url = `https://${url}`
+  }
+
+  if (Platform.OS === 'web') {
+    window.open(url, target)
+    return
+  }
+
+  Linking.openURL(url)
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#0000EE',
+  },
+})

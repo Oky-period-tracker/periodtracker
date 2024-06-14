c4ca21a613838759ea608a80ecde07eed8cad9d2
Alex Mitchell <amitchell@unicef.org>
2024-06-14 10:08:41 +0700

    Remove comment

--- packages/app/src/components/DatePicker.tsx
+++ packages/app/src/components/DatePicker.tsx
@@ -88,28 +88,3 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
 });
-
-/* 
-export interface MarkingProps extends DotProps {
-  type?: MarkingTypes;
-  theme?: Theme;
-  selected?: boolean;
-  marked?: boolean;
-  today?: boolean;
-  disabled?: boolean;
-  inactive?: boolean;
-  disableTouchEvent?: boolean;
-  activeOpacity?: number;
-  textColor?: string;
-  selectedColor?: string;
-  selectedTextColor?: string;
-  customTextStyle?: StyleProp<TextStyle>;
-  customContainerStyle?: StyleProp<ViewStyle>;
-  dotColor?: string;
-  dots?: DOT[];
-  periods?: PERIOD[];
-  startingDay?: boolean;
-  endingDay?: boolean;
-  accessibilityLabel?: string;
-  customStyles?: CustomStyle;
-} */

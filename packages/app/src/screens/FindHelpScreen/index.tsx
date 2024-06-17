3c55d8f9cde62bcc70ba31cbc48c3c6ba60d6050
Alex Mitchell <amitchell@unicef.org>
2024-06-17 18:23:22 +0700

    Add search to HelpCenters

--- packages/app/src/screens/FindHelpScreen/index.tsx
+++ packages/app/src/screens/FindHelpScreen/index.tsx
@@ -1,24 +1,40 @@
 import * as React from "react";
-import { View, StyleSheet, Text, ScrollView } from "react-native";
+import { StyleSheet, ScrollView } from "react-native";
 import { Screen } from "../../components/Screen";
 import { HelpCenter, data } from "../../data/data";
-import { A } from "../../components/A";
 import { ScreenComponent } from "../../navigation/RootNavigator";
 import { HelpCenterCard } from "./components/HelpCenterCard";
+import { Input } from "../../components/Input";
+import { useSearch } from "../../hooks/useSearch";
 
 const FindHelpScreen: ScreenComponent<"Help"> = () => {
-  const helpCenters = data.helpCenters;
+  const { query, setQuery, results } = useSearch<HelpCenter>({
+    options: helpCenters,
+    keys: searchKeys,
+  });
 
   return (
     <Screen>
       <ScrollView style={styles.scrollView}>
-        {helpCenters.map((item) => (
+        <Input value={query} onChangeText={setQuery} placeholder={"search"} />
+        {results.map((item) => (
           <HelpCenterCard key={`help-center-${item.id}`} helpCenter={item} />
         ))}
       </ScrollView>
     </Screen>
   );
 };
+
+// TODO: get help centers from redux
+const helpCenters = data.helpCenters;
+
+const searchKeys = [
+  "title" as const,
+  "caption" as const,
+  "address" as const,
+  "websites" as const,
+  // TODO: Add string of combined attribute names to HelpCenter type for use here in search
+];
 
 export default FindHelpScreen;
 

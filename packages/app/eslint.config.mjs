4a47b4e9da5768055e4f40b5ae890c1555c6c16b
Alex Mitchell <amitchell@unicef.org>
2024-06-18 08:54:23 +0700

    ignore expo

--- packages/app/eslint.config.mjs
+++ packages/app/eslint.config.mjs
@@ -4,22 +4,23 @@ import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { files: ["**/*.jsx"], languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  {
+    files: ["**/*.jsx"],
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+  },
   ...fixupConfigRules(pluginReactConfig),
   {
     files: ["**/*.jsx", "**/*.tsx"],
     rules: {
       "react/prop-types": "off",
-      "react/jsx-key":"off",
-      "react/no-unescaped-entities":"off"
-
+      "react/jsx-key": "off",
+      "react/no-unescaped-entities": "off",
     },
-  }
-
+  },
+  { ignores: [".expo"] },
 ];

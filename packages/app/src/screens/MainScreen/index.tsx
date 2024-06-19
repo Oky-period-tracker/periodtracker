bc1b780a26f3d3269dfabf79444b19e084b4ab0b
Alex Mitchell <amitchell@unicef.org>
2024-06-19 10:40:23 +0700

    Wheel WIP - measure drag displacement

--- packages/app/src/screens/MainScreen/index.tsx
+++ packages/app/src/screens/MainScreen/index.tsx
@@ -1,41 +1,48 @@
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
-import { Carousel } from "../../components/Carousel";
-import { Circle } from "../../components/icons/Circle";
-import { Cloud } from "../../components/icons/Cloud";
-import { Star } from "../../components/icons/Star";
+// import { Carousel } from "../../components/Carousel";
+// import { Circle } from "../../components/icons/Circle";
+// import { Cloud } from "../../components/icons/Cloud";
+// import { Star } from "../../components/icons/Star";
 import { ScreenComponent } from "../../navigation/RootNavigator";
-import { Button } from "../../components/Button";
-import { DailyCard } from "../../components/DailyCard";
-import { DayModal } from "../../components/DayModal";
-import { useToggle } from "../../hooks/useToggle";
+// import { Button } from "../../components/Button";
+// import { DailyCard } from "../../components/DailyCard";
+// import { DayModal } from "../../components/DayModal";
+// import { useToggle } from "../../hooks/useToggle";
+import { Wheel } from "./components/Wheel";
 
-const data = [{}, {}, {}, {}, {}, {}, {}, {}];
+// const data = [{}, {}, {}, {}, {}, {}, {}, {}];
 
-const MainScreen: ScreenComponent<"Home"> = ({ navigation }) => {
-  const goToCalendar = () => navigation.navigate("Calendar");
-  const goToDay = () => navigation.navigate("Day");
+const MainScreen: ScreenComponent<"Home"> = (/* { navigation } */) => {
+  // const goToCalendar = () => navigation.navigate("Calendar");
+  // const goToDay = () => navigation.navigate("Day");
 
-  const [visible, toggleVisible] = useToggle();
+  // const [visible, toggleVisible] = useToggle();
 
   return (
     <View style={styles.screen}>
-      <Button
-        status={"secondary"}
-        style={styles.button}
-        onPress={goToCalendar}
-      ></Button>
-
-      <Cloud size={100} />
-      <Circle size={100} />
-      <Star size={100} />
-
-      <View style={styles.carouselContainer}>
-        <Carousel data={data} CardComponent={DailyCard} onCardPress={goToDay} />
-        <DayModal {...{ visible, toggleVisible }} />
-      </View>
+      <Wheel />
     </View>
   );
+
+  // return (
+  //   <View style={styles.screen}>
+  //     <Button
+  //       status={"secondary"}
+  //       style={styles.button}
+  //       onPress={goToCalendar}
+  //     ></Button>
+
+  //     <Cloud size={100} />
+  //     <Circle size={100} />
+  //     <Star size={100} />
+
+  //     <View style={styles.carouselContainer}>
+  //       <Carousel data={data} CardComponent={DailyCard} onCardPress={goToDay} />
+  //       <DayModal {...{ visible, toggleVisible }} />
+  //     </View>
+  //   </View>
+  // );
 };
 
 export default MainScreen;

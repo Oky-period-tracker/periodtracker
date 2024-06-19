de2a3dd3d57a6aa99d48c4f19dfac735429bee8b
Alex Mitchell <amitchell@unicef.org>
2024-06-19 12:06:17 +0700

    Rotate based on vertical drag distance

--- packages/app/src/screens/MainScreen/index.tsx
+++ packages/app/src/screens/MainScreen/index.tsx
@@ -50,6 +50,8 @@ export default MainScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
   carouselContainer: {
     marginTop: "auto",

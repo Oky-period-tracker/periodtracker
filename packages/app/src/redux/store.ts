899a3922d4ff84c511d6d9bea97baad0c0595bf3
Alex Mitchell <amitchell@unicef.org>
2024-06-28 12:51:06 +0700

    Add saga middleware

--- packages/app/src/redux/store.ts
+++ packages/app/src/redux/store.ts
@@ -1,9 +1,11 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { encryptTransform } from "redux-persist-transform-encrypt";
-import { config } from "./config";
+import createSagaMiddleware from "redux-saga";
 import { rootReducer } from "./reducers";
+import { rootSaga } from "./sagas";
+import { config } from "./config";
 
 const encryptor = encryptTransform({
   secretKey: config.REDUX_ENCRYPT_KEY,
@@ -23,8 +25,13 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = createStore(persistedReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+
 const persistor = persistStore(store);
+
+sagaMiddleware.run(rootSaga);
 
 export { store, persistor };
 

ef2115a65986069d8bdf906555f87437d9bf6b43
Alex Mitchell <amitchell@unicef.org>
2024-07-03 15:57:32 +0700

    Add pie chart to circle progress

--- packages/app/src/screens/MainScreen/components/CircleProgress.tsx
+++ packages/app/src/screens/MainScreen/components/CircleProgress.tsx
@@ -3,28 +3,47 @@ import { StyleSheet } from "react-native";
 import { Button, ButtonProps } from "../../../components/Button";
 import { Text } from "../../../components/Text";
 import { useTodayPrediction } from "../../../contexts/PredictionProvider";
+import PieChart from "react-native-pie-chart";
 
-export const CircleProgress = ({ onPress }: ButtonProps) => {
-  const { cycleLength } = useTodayPrediction();
+export const CircleProgress = ({
+  size = 52,
+  onPress,
+}: ButtonProps & { size?: number }) => {
+  const { cycleDay, cycleLength } = useTodayPrediction();
+  const series = [cycleDay, cycleLength - cycleDay];
 
   return (
-    <Button status={"secondary"} style={styles.button} onPress={onPress}>
+    <Button
+      onPress={onPress}
+      status={"secondary"}
+      style={{
+        width: size,
+        height: size,
+      }}
+    >
+      <PieChart
+        widthAndHeight={size}
+        series={series}
+        sliceColor={sliceColor}
+        style={styles.pie}
+      />
       <Text style={styles.numberText}>
         {cycleLength === 100 ? "-" : cycleLength}
       </Text>
-      <Text style={styles.dayText}>days</Text>
+      <Text>days</Text>
     </Button>
   );
 };
 
+const sliceColor = ["#fff", "transparent"];
+
 const styles = StyleSheet.create({
-  button: {
-    width: 52,
-    height: 52,
-  },
   numberText: {
     fontSize: 16,
     fontWeight: "bold",
   },
-  dayText: {},
+  pie: {
+    position: "absolute",
+    opacity: 0.5,
+  },
 });

5afa741953c3939e64d826a35867f2085873700a
Alex Mitchell <amitchell@unicef.org>
2024-07-13 09:01:29 +0200

    Use random DYK from redux

--- packages/app/src/screens/DayScreen/components/DayTracker/DidYouKnowCard.tsx
+++ packages/app/src/screens/DayScreen/components/DayTracker/DidYouKnowCard.tsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useSelector } from "../../../../redux/useSelector";
+import { allDidYouKnowsSelectors } from "../../../../redux/selectors";
+import _ from "lodash";
 
 export const DidYouKnowCard = () => {
-  // TODO: Get random DYK entry from redux
-  const item = {
-    id: "4416e050-d4d5-4313-a268-410410021668",
-    isAgeRestricted: false,
-    title: "Menstruation and menstrual cycle",
-    content:
-      "In regular cycles, ovulation happens 10–15 days before your next period. 📆",
-    live: true,
-  };
+  const allDidYouKnows = useSelector(allDidYouKnowsSelectors);
+  const randomDidYouKnow = React.useMemo(() => {
+    return _.sample(allDidYouKnows);
+  }, []);
 
   return (
     <View style={styles.page}>
       <Text style={styles.title}>Did you know?</Text>
-      <Text>Learn a new fact about your body every day!</Text>
+      <Text>{randomDidYouKnow?.title}</Text>
       <View style={styles.body}>
-        <Text style={styles.content}>{item.content}</Text>
+        <Text style={styles.content}>{randomDidYouKnow?.content}</Text>
       </View>
     </View>
   );

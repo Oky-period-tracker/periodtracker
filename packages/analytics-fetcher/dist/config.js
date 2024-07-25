19f3bc889193b392f2c2440aac2db56ab0ab95f8
Riya Sidana <riyasidana1504@gmail.com>
2024-07-25 21:38:32 +0530

    node app for cms

---
+++ packages/analytics-fetcher/dist/config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.cmsConfig = void 0;
+exports.cmsConfig = {
+    cms: {
+        loginUrl: 'http://localhost:5000/login',
+        dataUrl: 'http://localhost:5000/analytics-management',
+        credentials: {
+            username: 'admin',
+            password: 'admin',
+        },
+    }
+};

6743c15d5ee3607c6a5e0b5916153275cf41045b
Alex Mitchell <amitchell@unicef.org>
2024-07-26 18:45:23 +0100

    Fix cloud alignment in tutorial

--- packages/app/src/screens/MainScreen/components/TutorialFeature.tsx
+++ packages/app/src/screens/MainScreen/components/TutorialFeature.tsx
@@ -129,6 +129,7 @@ const styles = StyleSheet.create({
     margin: 8,
     flexDirection: "column",
     alignItems: "center",
+    justifyContent: "space-between",
   },
   cloudText: {
     textAlign: "center",

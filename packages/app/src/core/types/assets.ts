f5a875163a94aaa577e7bf9a5bc38aa52919144a
Alex Mitchell <amitchell@unicef.org>
2024-07-31 22:52:38 +0100

    Remove unused assets

--- packages/app/src/core/types/assets.ts
+++ packages/app/src/core/types/assets.ts
@@ -1,5 +1,5 @@
 import { ImageSourcePropType } from "react-native";
-import { AnimationObject, LottieViewProps } from "lottie-react-native";
+import { AnimationObject } from "lottie-react-native";
 import { AvatarName, Locale, ThemeName } from "../modules/translations";
 
 export interface AppAssets {
@@ -7,8 +7,6 @@ export interface AppAssets {
     AvatarName,
     {
       default: ImageSourcePropType;
-      small: ImageSourcePropType;
-      stationary: ImageSourcePropType;
       stationary_colour: ImageSourcePropType;
       bubbles: ImageSourcePropType;
       theme: ImageSourcePropType;
@@ -27,48 +25,6 @@ export interface AppAssets {
     spin_load_face: ImageSourcePropType;
     spin_load_circle: ImageSourcePropType;
     icons: {
-      settingsIcon: ImageSourcePropType;
-      cloudsIcn: ImageSourcePropType;
-      information: ImageSourcePropType;
-      down: ImageSourcePropType;
-      Village: ImageSourcePropType;
-      VillageGrey: ImageSourcePropType;
-      exclamation: ImageSourcePropType;
-      starLine: ImageSourcePropType;
-      fullHeart: ImageSourcePropType;
-      circleDefaultL: ImageSourcePropType;
-      arrow: ImageSourcePropType;
-      close: ImageSourcePropType;
-      infoPink: ImageSourcePropType;
-      infoBlue: ImageSourcePropType;
-      closeLine: ImageSourcePropType;
-      edit: ImageSourcePropType;
-      Male: ImageSourcePropType;
-      MaleGrey: ImageSourcePropType;
-      Female: ImageSourcePropType;
-      FemaleGrey: ImageSourcePropType;
-      Other: ImageSourcePropType;
-      OtherGrey: ImageSourcePropType;
-      Urban: ImageSourcePropType;
-      UrbanGrey: ImageSourcePropType;
-      Rural: ImageSourcePropType;
-      RuralGrey: ImageSourcePropType;
-      tick: ImageSourcePropType;
-      news: ImageSourcePropType;
-      back: ImageSourcePropType;
-      calendar: ImageSourcePropType;
-      calendarL: ImageSourcePropType;
-      genderL: ImageSourcePropType;
-      locationL: ImageSourcePropType;
-      lockL: ImageSourcePropType;
-      shieldL: ImageSourcePropType;
-      profile: ImageSourcePropType;
-      profileL: ImageSourcePropType;
-      profileGuest: ImageSourcePropType;
-      undoOval: ImageSourcePropType;
-      switch: ImageSourcePropType;
-      send: ImageSourcePropType;
-      periodFuture: ImageSourcePropType;
       starOrange: {
         empty: ImageSourcePropType;
         half: ImageSourcePropType;
@@ -79,55 +35,10 @@ export interface AppAssets {
         half: ImageSourcePropType;
         full: ImageSourcePropType;
       };
-      roundedMask: ImageSourcePropType;
-      cycle: ImageSourcePropType;
-      periodLength: ImageSourcePropType;
-      periodDays: ImageSourcePropType;
-      tabs: {
-        encyclopedia: ImageSourcePropType;
-        encyclopediaGrey: ImageSourcePropType;
-        main: ImageSourcePropType;
-        mainGrey: ImageSourcePropType;
-        profile: ImageSourcePropType;
-        profileGrey: ImageSourcePropType;
-        settings: ImageSourcePropType;
-        settingsGrey: ImageSourcePropType;
-      };
-      clouds: {
-        nonPeriod: ImageSourcePropType;
-        fertile: ImageSourcePropType;
-        period: ImageSourcePropType;
-        notVerifiedDay: ImageSourcePropType;
-      };
-      stars: {
-        notVerifiedDay: ImageSourcePropType;
-        nonPeriod: ImageSourcePropType;
-        fertile: ImageSourcePropType;
-        period: ImageSourcePropType;
-      };
-      circles: {
-        notVerifiedDay: ImageSourcePropType;
-        nonPeriod: ImageSourcePropType;
-        fertile: ImageSourcePropType;
-        period: ImageSourcePropType;
-      };
-      segment: {
-        notVerifiedDay: ImageSourcePropType;
-        nonPeriod: ImageSourcePropType;
-        fertile: ImageSourcePropType;
-        period: ImageSourcePropType;
-      };
-    };
-    dayBadge: {
-      notVerifiedDay: ImageSourcePropType;
-      onPeriod: ImageSourcePropType;
-      onFertile: ImageSourcePropType;
-      default: ImageSourcePropType;
     };
   };
   general: {
     aboutBanner: Record<Locale, ImageSourcePropType>;
-    calendarStatic: Record<Locale, ImageSourcePropType>;
   };
   lottie: {
     avatars: Record<AvatarName, AnimationObject>;

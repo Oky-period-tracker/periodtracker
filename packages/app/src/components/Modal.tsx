19bd6f4b5a43f37f05ea30ff9bbc8466eb70d0e1
Alex Mitchell <amitchell@unicef.org>
2024-08-08 12:43:28 +0100

    Add X button to all modals

--- packages/app/src/components/Modal.tsx
+++ packages/app/src/components/Modal.tsx
@@ -6,9 +6,12 @@ import {
   View,
   ViewStyle,
 } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useScreenDimensions } from "../hooks/useScreenDimensions";
 import { IS_WEB } from "../services/device";
+import { useAccessibilityLabel } from "../hooks/useAccessibilityLabel";
+import { Button, ButtonProps } from "./Button";
 
 export type ModalProps = {
   visible: boolean;
@@ -38,6 +41,7 @@ export const Modal = ({
     >
       <View style={styles.container}>
         <TouchableOpacity style={styles.backDrop} onPress={toggleVisible} />
+        <ModalCloseButton onPress={toggleVisible} />
         <SafeAreaView
           style={[styles.children, { maxWidth, maxHeight }, style]}
           pointerEvents="box-none"
@@ -46,6 +50,22 @@ export const Modal = ({
         </SafeAreaView>
       </View>
     </RNModal>
+  );
+};
+
+export const ModalCloseButton = (props: ButtonProps) => {
+  const getAccessibilityLabel = useAccessibilityLabel();
+  const label = getAccessibilityLabel("close");
+
+  return (
+    <Button style={styles.closeButton} status={"basic"} {...props}>
+      <FontAwesome
+        name="close"
+        size={24}
+        color="white"
+        accessibilityLabel={label}
+      />
+    </Button>
   );
 };
 
@@ -62,5 +82,13 @@ const styles = StyleSheet.create({
   },
   children: {
     margin: 24,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 60,
+    right: 24,
+    width: 32,
+    height: 32,
+    zIndex: 9999,
   },
 });

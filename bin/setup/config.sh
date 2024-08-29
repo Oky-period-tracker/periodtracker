34d3441cd3a11115cd2d16e7e63e0e2fa93a7745
Alex Mitchell <amitchell@unicef.org>
2024-08-29 18:17:34 +0100

    Only copy-config file if doesnt exist

--- bin/setup/config.sh
+++ bin/setup/config.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
+# Function to check if .env exists, and if not, copy from .env.dist
+copy_if_not_exists() {
+  if [ ! -f "$2" ]; then
+    cp "$1" "$2"
+    echo "Copied $2"
+  else
+    echo "Skipped $2"
+  fi
+}
+
+
 # Submodule URLs
-cp bin/modules/urls.sh.dist bin/modules/urls.sh
+copy_if_not_exists bin/modules/urls.sh.dist bin/modules/urls.sh
 
 # env
-cp packages/api/.env.dist packages/api/.env
-cp packages/cms/.env.dist packages/cms/.env
-cp app/.env.dist app/.env
+copy_if_not_exists packages/api/.env.dist packages/api/.env
+copy_if_not_exists packages/cms/.env.dist packages/cms/.env
+copy_if_not_exists app/.env.dist app/.env
 
 # iOS
 # cp app/ios/xcconfig.dist app/ios/release.xcconfig
 # cp app/ios/xcconfig.dist app/ios/debug.xcconfig
 
 # Android
-cp app/android/gradle.properties.dist app/android/gradle.properties
-cp app/android/local.properties.dist app/android/local.properties
+copy_if_not_exists app/android/gradle.properties.dist app/android/gradle.properties
+copy_if_not_exists app/android/local.properties.dist app/android/local.properties
 
 # Redux encryption
-cp app/src/redux/config.ts.dist app/src/redux/config.ts
+copy_if_not_exists app/src/redux/config.ts.dist app/src/redux/config.ts

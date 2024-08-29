b05798de434e98539566c849b93d444eb552c773
Alex Mitchell <amitchell@unicef.org>
2024-08-29 18:17:20 +0100

    Update copy config files

--- bin/setup/config.sh
+++ bin/setup/config.sh
@@ -6,16 +6,15 @@ cp bin/modules/urls.sh.dist bin/modules/urls.sh
 # env
 cp packages/api/.env.dist packages/api/.env
 cp packages/cms/.env.dist packages/cms/.env
-cp packages/mobile/.env.dist packages/mobile/.env
-cp packages/mobile/.env.dist packages/mobile/.env.production
+cp app/.env.dist app/.env
 
 # iOS
-cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/release.xcconfig
-cp packages/mobile/ios/xcconfig.dist packages/mobile/ios/debug.xcconfig
+# cp app/ios/xcconfig.dist app/ios/release.xcconfig
+# cp app/ios/xcconfig.dist app/ios/debug.xcconfig
 
 # Android
-cp packages/mobile/android/gradle.properties.dist packages/mobile/android/gradle.properties
-cp packages/mobile/android/local.properties.dist packages/mobile/android/local.properties
+cp app/android/gradle.properties.dist app/android/gradle.properties
+cp app/android/local.properties.dist app/android/local.properties
 
 # Redux encryption
-cp packages/components/src/redux/config.ts.dist packages/components/src/redux/config.ts
+cp app/src/redux/config.ts.dist app/src/redux/config.ts

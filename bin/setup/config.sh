8c0b0d37066d02eabd13414dd69b3433156c2ec1
Alex Mitchell <amitchell@unicef.org>
2024-09-01 23:43:28 +0100

    Remove old setup scripts

--- bin/setup/config.sh
+++ bin/setup/config.sh
@@ -19,17 +19,5 @@ copy_if_not_exists packages/api/.env.dist packages/api/.env
 copy_if_not_exists packages/cms/.env.dist packages/cms/.env
 copy_if_not_exists app/.env.dist app/.env
 
-# iOS
-# cp app/ios/xcconfig.dist app/ios/release.xcconfig
-# cp app/ios/xcconfig.dist app/ios/debug.xcconfig
-
-# Android
-copy_if_not_exists app/android/gradle.properties.dist app/android/gradle.properties
-copy_if_not_exists app/android/local.properties.dist app/android/local.properties
-
-# Redux encryption
-copy_if_not_exists app/src/redux/config.ts.dist app/src/redux/config.ts
-
 # Expo
-copy_if_not_exists app/app.json.dist app/app.json
 copy_if_not_exists app/eas.json.dist app/eas.json

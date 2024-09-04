f9eaa6f4f25619f63eb77694483d0f87d13b3f45
Alex Mitchell <amitchell@unicef.org>
2024-09-04 20:26:18 +0100

    Remove commented legacy code

--- app/src/redux/sagas/contentSaga.ts
+++ app/src/redux/sagas/contentSaga.ts
@@ -1,7 +1,6 @@
 import { all, put, select, takeLatest } from 'redux-saga/effects'
 import { RehydrateAction, REHYDRATE } from 'redux-persist'
 import { ExtractActionFromActionType } from '../types'
-// import { liveContent as staleContent } from "@oky/core";
 import { httpClient } from '../../services/HttpClient'
 import * as selectors from '../selectors'
 import * as actions from '../actions'
@@ -14,7 +13,6 @@ import {
   fromHelpCenters,
   fromQuizzes,
 } from '../../mappers'
-// import messaging from '@react-native-firebase/messaging' TODO:
 
 function* onRehydrate(action: RehydrateAction) {
   // @ts-expect-error TODO:
@@ -229,12 +227,6 @@ function* onFetchContentRequest(action: ExtractActionFromActionType<'FETCH_CONTE
 function* onSetLocale(action: ExtractActionFromActionType<'SET_LOCALE'>) {
   const { locale } = action.payload
 
-  // unsubscribe from topic
-  // TODO:: use locales from submodule
-  // messaging().unsubscribeFromTopic("oky_en_notifications");
-  // messaging().unsubscribeFromTopic("oky_id_notifications");
-  // messaging().unsubscribeFromTopic("oky_mn_notifications");
-  // messaging().subscribeToTopic(`oky_${locale}_notifications`);
   // @ts-expect-error TODO:
   yield put(actions.initStaleContent(staleContent[locale]))
 

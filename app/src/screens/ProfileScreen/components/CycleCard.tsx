852105015a2560d3cb47a39f2d8820cbc1b9abc5
Alex Mitchell <amitchell@unicef.org>
2024-09-10 11:54:41 +0100

    Fix cycle card styles

--- app/src/screens/ProfileScreen/components/CycleCard.tsx
+++ app/src/screens/ProfileScreen/components/CycleCard.tsx
@@ -44,91 +44,94 @@ export const CycleCard = ({
   const periodEndMonth = months[periodEndMonthIndex]
 
   return (
-    <View style={[styles.cycleCard, globalStyles.shadow]}>
-      {/* ===== Header ===== */}
-      <View style={styles.cycleCardHeader}>
-        <View style={styles.row}>
-          <Text style={styles.headerText}>cycle</Text>
-          <Text
-            enableTranslate={false}
-            style={[styles.headerText, styles.bold]}
-          >{` ${cycleNumber}`}</Text>
-        </View>
-
-        <View style={styles.row}>
-          <Text enableTranslate={false} style={[styles.headerText, styles.bold]}>
-            {`${item.cycleLength} `}
-          </Text>
-          <Text style={styles.headerText}>day_cycle</Text>
-        </View>
-
-        <View style={styles.row}>
-          <Text enableTranslate={false} style={[styles.headerText, styles.bold]}>
-            {startDay}
-          </Text>
-          <Text enableTranslate={false} style={styles.headerText}>{` ${startMonth} - `}</Text>
-          <Text enableTranslate={false} style={[styles.headerText, styles.bold]}>
-            {endDay}
-          </Text>
-          <Text enableTranslate={false} style={styles.headerText}>{` ${endMonth}`}</Text>
-        </View>
-      </View>
-
-      {/* ===== Body ===== */}
-      <View style={styles.cycleCardBody}>
-        <View style={styles.cycleCardBodyLeft}>
+    <View style={[styles.container, globalStyles.shadow]}>
+      <View style={styles.cycleCard}>
+        {/* ===== Header ===== */}
+        <View style={styles.cycleCardHeader}>
           <View style={styles.row}>
-            <Text enableTranslate={false} style={styles.bold}>
-              {`${item.periodLength} `}
-            </Text>
-            <Text>day_period</Text>
+            <Text style={styles.headerText}>cycle</Text>
+            <Text
+              enableTranslate={false}
+              style={[styles.headerText, styles.bold]}
+            >{` ${cycleNumber}`}</Text>
           </View>
 
           <View style={styles.row}>
-            <Text enableTranslate={false} style={styles.bold}>
+            <Text enableTranslate={false} style={[styles.headerText, styles.bold]}>
+              {`${item.cycleLength} `}
+            </Text>
+            <Text style={styles.headerText}>day_cycle</Text>
+          </View>
+
+          <View style={styles.row}>
+            <Text enableTranslate={false} style={[styles.headerText, styles.bold]}>
               {startDay}
             </Text>
-            <Text enableTranslate={false}>{` ${startMonth} - `}</Text>
-            <Text enableTranslate={false} style={styles.bold}>
-              {periodEndDay}
+            <Text enableTranslate={false} style={styles.headerText}>{` ${startMonth} - `}</Text>
+            <Text enableTranslate={false} style={[styles.headerText, styles.bold]}>
+              {endDay}
             </Text>
-            <Text enableTranslate={false}>{` ${periodEndMonth}`}</Text>
+            <Text enableTranslate={false} style={styles.headerText}>{` ${endMonth}`}</Text>
           </View>
         </View>
-        <View style={styles.cycleCardBodyRight}>
-          {Object.entries(emojiOptions).map(([key]) => {
-            // @ts-expect-error TODO:
-            const isArray = Array.isArray(cardAnswersValues[key])
 
-            const isEmojiActive = isArray
-              ? // @ts-expect-error TODO:
-                cardAnswersValues[key]?.length > 0
-              : // @ts-expect-error TODO:
-                !!cardAnswersValues[key]
+        {/* ===== Body ===== */}
+        <View style={styles.cycleCardBody}>
+          <View style={styles.cycleCardBodyLeft}>
+            <Text>
+              <Text enableTranslate={false} style={styles.bold}>
+                {`${item.periodLength} `}
+              </Text>
+              <Text>day_period</Text>
+            </Text>
 
-            const answer = isEmojiActive
-              ? isArray
+            <Text>
+              <Text enableTranslate={false} style={styles.bold}>
+                {startDay}
+              </Text>
+              <Text enableTranslate={false}>{` ${startMonth} - `}</Text>
+              <Text enableTranslate={false} style={styles.bold}>
+                {periodEndDay}
+              </Text>
+              <Text enableTranslate={false}>{` ${periodEndMonth}`}</Text>
+            </Text>
+          </View>
+          <View style={styles.cycleCardBodyRight}>
+            {Object.entries(emojiOptions).map(([key]) => {
+              // @ts-expect-error TODO:
+              const isArray = Array.isArray(cardAnswersValues[key])
+
+              const isEmojiActive = isArray
                 ? // @ts-expect-error TODO:
-                  cardAnswersValues[key][0]
+                  cardAnswersValues[key]?.length > 0
                 : // @ts-expect-error TODO:
-                  cardAnswersValues[key]
-              : ''
+                  !!cardAnswersValues[key]
 
-            const emoji = isEmojiActive
-              ? // @ts-expect-error TODO:
-                emojiOptions[key][answer]
-              : defaultEmoji
+              const answer = isEmojiActive
+                ? isArray
+                  ? // @ts-expect-error TODO:
+                    cardAnswersValues[key][0]
+                  : // @ts-expect-error TODO:
+                    cardAnswersValues[key]
+                : ''
 
-            return (
-              <EmojiBadge
-                key={`${item.cycleStartDate}-${key}`}
-                emoji={emoji}
-                text={translate(key)}
-                status={isEmojiActive ? 'danger' : 'basic'}
-                disabled
-              />
-            )
-          })}
+              const emoji = isEmojiActive
+                ? // @ts-expect-error TODO:
+                  emojiOptions[key][answer]
+                : defaultEmoji
+
+              return (
+                <EmojiBadge
+                  key={`${item.cycleStartDate}-${key}`}
+                  emoji={emoji}
+                  text={translate(key)}
+                  status={isEmojiActive ? 'danger' : 'basic'}
+                  size={'small'}
+                  disabled
+                />
+              )
+            })}
+          </View>
         </View>
       </View>
     </View>
@@ -136,12 +139,17 @@ export const CycleCard = ({
 }
 
 const styles = StyleSheet.create({
+  container: {
+    borderRadius: 20,
+    width: '100%',
+    height: 140,
+    marginVertical: 4,
+  },
   cycleCard: {
     backgroundColor: '#fff',
     borderRadius: 20,
     width: '100%',
     height: 140,
-    marginVertical: 4,
     flexDirection: 'column',
     overflow: 'hidden',
   },
@@ -156,7 +164,6 @@ const styles = StyleSheet.create({
   },
   headerText: {
     color: '#fff',
-    fontSize: 16,
   },
   cycleCardBody: {
     flexDirection: 'row',
@@ -165,13 +172,13 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   cycleCardBodyLeft: {
-    flex: 2,
     height: '100%',
     flexDirection: 'column',
     justifyContent: 'space-around',
+    flexBasis: '50%',
   },
   cycleCardBodyRight: {
-    flex: 3,
+    flexBasis: '50%',
     height: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',

7ca71b27974090b72dfe0b825e1759b788eca311
Alex Mitchell <amitchell@unicef.org>
2024-09-27 17:51:59 +0100

    Dont copy non-existent folder

--- packages/cms/copyStaticAssets.ts
+++ packages/cms/copyStaticAssets.ts
@@ -3,10 +3,10 @@ import * as shell from 'shelljs'
 const syncFolders: any = {
   './src/views': './dist/src/views',
   './src/public': './dist/src/public',
-  './src/i18n': './dist/src/i18n',
+  // './src/i18n': './dist/src/i18n',
 }
 
-Object.keys(syncFolders).map(sourceFolder => {
+Object.keys(syncFolders).map((sourceFolder) => {
   const distFolder = syncFolders[sourceFolder]
 
   shell.rm('-rf', distFolder)

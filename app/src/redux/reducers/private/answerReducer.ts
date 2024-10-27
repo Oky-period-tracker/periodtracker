1895da0bb9b8453a1bda622dd2fb8093709fadd3
Alex Mitchell <amitchell@unicef.org>
2024-10-27 10:28:45 +0700

    Bring back answerReducer refactored

--- app/src/redux/reducers/private/answerReducer.ts
+++ app/src/redux/reducers/private/answerReducer.ts
@@ -1,5 +1,7 @@
 import { Actions } from '../../types'
 import { DailyCard } from '../../../types'
+import { AllSurveys, CompletedSurveys } from '../../../core/types'
+import { toShortISO } from '../../../services/dateUtils'
 
 export interface VerifiedDates {
   [utcShortISO: string]: {
@@ -9,6 +11,8 @@ export interface VerifiedDates {
 }
 
 export interface AnswerState {
+  allSurveys: AllSurveys
+  completedSurveys: CompletedSurveys
   quizzes: {
     [id: string]: {
       id: string
@@ -34,6 +38,8 @@ export interface AnswerState {
 }
 
 const initialState: AnswerState = {
+  allSurveys: [],
+  completedSurveys: [],
   quizzes: {},
   cards: {},
   verifiedDates: {},
@@ -53,24 +59,103 @@ export function answerReducer(state: AnswerState = initialState, action: Actions
     case 'ANSWER_QUIZ': {
       return {
         ...state,
+        quizzes: {
+          ...state.quizzes,
+          [action.payload.id]: {
+            id: action.payload.id,
+            question: action.payload.question,
+            // @ts-expect-error TODO:
+            answerID: action.payload.answerID,
+            emoji: action.payload.emoji,
+            answer: action.payload.answer,
+            isCorrect: action.payload.isCorrect,
+            response: action.payload.response,
+            utcDateTime: action.payload.utcDateTime.toISOString(),
+          },
+        },
       }
     }
 
     case 'ANSWER_DAILY_CARD': {
+      const keyCard = toShortISO(action.payload.utcDateTime)
+      let answersToInsert: string[] = []
+      // Added as a way of handling multiple selections and to account for the initial release of single selections (Painful, I know)
+
+      if (
+        state.cards[keyCard] !== undefined &&
+        state.cards[keyCard][action.payload.cardName] &&
+        !action.payload.mutuallyExclusive &&
+        action.payload.cardName !== 'periodDay'
+      ) {
+        if (!action.payload.answer || action.payload.answer === true) {
+          return state
+        }
+
+        if (typeof state.cards[keyCard][action.payload.cardName] === 'string') {
+          // This is to account for old data that used to just be a string and now we need to have multiple
+          // we put that string as part of an array before concatenating the new answers
+          const legacyData = state.cards[keyCard][action.payload.cardName]
+          const baseArray = legacyData ? [legacyData] : []
+          answersToInsert = baseArray.concat(action.payload.answer)
+        } else {
+          if (state.cards?.[keyCard]?.[action.payload.cardName]?.includes(action.payload.answer)) {
+            // Remove if already contained (toggle ability)
+            // @ts-expect-error TODO:
+            answersToInsert = state.cards?.[keyCard]?.[action.payload.cardName]?.filter(
+              // @ts-expect-error TODO:
+              (item) => item !== action.payload.answer,
+            )
+          } else {
+            // @ts-expect-error TODO:
+            answersToInsert = state.cards?.[keyCard]?.[action.payload.cardName]?.concat(
+              action.payload.answer,
+            )
+          }
+        }
+      } else {
+        // @ts-expect-error TODO:
+        answersToInsert = [action.payload.answer]
+      }
+
       return {
         ...state,
+        cards: {
+          ...state.cards,
+          [keyCard]: {
+            ...(state.cards[keyCard] || {}),
+            [action.payload.cardName]: answersToInsert,
+          },
+        },
       }
     }
 
     case 'ANSWER_VERIFY_DATES': {
+      const keyCard = toShortISO(action.payload.utcDateTime)
       return {
         ...state,
+        verifiedDates: {
+          ...state.verifiedDates,
+          [keyCard]: {
+            ...(state.verifiedDates[keyCard] || {}),
+            periodDay: action.payload.periodDay,
+          },
+        },
       }
     }
 
     case 'ANSWER_NOTES_CARD': {
+      const keyCard = toShortISO(action.payload.utcDateTime)
       return {
         ...state,
+        // @ts-expect-error TODO:
+        notes: {
+          ...state.notes,
+          [keyCard]: {
+            title: action.payload.title,
+            notes: action.payload.notes,
+            utcDateTime: action.payload.utcDateTime,
+          },
+        },
       }
     }
 

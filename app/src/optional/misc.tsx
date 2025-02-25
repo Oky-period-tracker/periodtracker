2b74a649eb2bb52b90e987ed2ea8ac1107d2b771
Alex Mitchell <amitchell@unicef.org>
2025-02-25 15:59:48 +0700

    Add optional sign up emojis

--- app/src/optional/misc.tsx
+++ app/src/optional/misc.tsx
@@ -7,4 +7,16 @@ try {
   //
 }
 
-export { miscTranslations }
+let genders = [
+  { value: 'Male', label: 'Male', iconName: 'male' },
+  { value: 'Female', label: 'Female', iconName: 'female' },
+  { value: 'Other', label: 'Other', iconName: 'genderless' },
+]
+
+try {
+  genders = require('../resources/translations/misc').genders
+} catch (e) {
+  //
+}
+
+export { miscTranslations, genders }

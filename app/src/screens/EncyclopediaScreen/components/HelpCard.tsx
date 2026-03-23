0dd5ba61185fe72c21ae801b782f7994765c49e0
Antoine Hurard <antoine@reliefapplications.org>
2026-03-23 23:45:13 +0900

    fix help card

--- app/src/screens/EncyclopediaScreen/components/HelpCard.tsx
+++ app/src/screens/EncyclopediaScreen/components/HelpCard.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
-import { Image, StyleSheet, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Image, StyleSheet, TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
 import { Text } from '../../../components/Text'
 import { getAsset } from '../../../services/asset'
 import { useSelector } from 'react-redux'
 import { currentAvatarSelector } from '../../../redux/selectors'
 import { useColor } from '../../../hooks/useColor'
+import { useAvatar } from '../../../hooks/useAvatar'
+import { AvatarPreview } from '../../../components/AvatarPreview'
+import { assets } from '../../../resources/assets'
 
 export const HelpCard = ({ ...props }: TouchableOpacityProps) => {
   const selectedAvatar = useSelector(currentAvatarSelector)
   const { palette } = useColor()
+  const avatarPreviewData = useAvatar()
+
+  const isFriendAvatar = selectedAvatar === 'friend'
+  const isCustomAvatar = isFriendAvatar && avatarPreviewData !== null
+
+  if (isCustomAvatar) {
+    return (
+      <TouchableOpacity style={styles.helpCard} {...props}>
+        <View style={styles.customAvatarContainer}>
+          <View style={styles.cloudsWrapper}>
+            <Image
+              source={assets.static.clouds}
+              style={styles.cloudsBackground}
+              resizeMode="contain"
+            />
+          </View>
+          <View style={styles.avatarWrapper}>
+            <AvatarPreview
+              bodyType={avatarPreviewData.bodyType}
+              skinColor={avatarPreviewData.skinColor}
+              hairStyle={avatarPreviewData.hairStyle}
+              hairColor={avatarPreviewData.hairColor}
+              eyeShape={avatarPreviewData.eyeShape}
+              eyeColor={avatarPreviewData.eyeColor}
+              smile={avatarPreviewData.smile}
+              clothing={avatarPreviewData.clothing}
+              devices={avatarPreviewData.devices}
+              width={60}
+              height={80}
+            />
+          </View>
+          <Text style={[styles.customText, { color: palette.secondary.text }]}>find help</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
 
   return (
     <TouchableOpacity style={styles.helpCard} {...props}>
@@ -40,5 +79,37 @@ const styles = StyleSheet.create({
   },
   image: {
     height: 140,
+  },
+  customAvatarContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    width: 120,
+    height: 140,
+  },
+  cloudsWrapper: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  cloudsBackground: {
+    width: '100%',
+    maxHeight: '100%',
+  },
+  avatarWrapper: {
+    zIndex: 1,
+    marginBottom: 24,
+  },
+  customText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    position: 'absolute',
+    bottom: 0,
+    zIndex: 2,
   },
 })
